295eb2e0b05ed4330489037676c57ce953137e25
drogers <drogers@bridge2solutions.com>
2018-01-25 16:58:52 -0500

    base springboot

---
+++ src/main/java/com/kcorkrad/cardServer/CardServerApplication.java
@@ -0,0 +1,12 @@
+package com.kcorkrad.cardServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardServerApplication.class, args);
+	}
+}
