1731adfd2809c459be77c3c09366584164f3ddac
drogers <drogers@bridge2solutions.com>
2018-01-29 14:11:03 -0500

    edit proof

--- src/main/java/bookmarks/Application.java
+++ src/main/java/bookmarks/Application.java
@@ -26,7 +26,7 @@ public class Application {
                             bookmarkRepository.save(new Bookmark(account,
                                     "http://bookmark.com/1/" + a, "A description"));
                             bookmarkRepository.save(new Bookmark(account,
-                                    "http://bookmark.com/2/" + a, "A description"));
+                                    "http://bookmark.com/2/" + a, "A description proof of concept"));
                             if (a.equals("andrea"))
                                 bookmarkRepository.save(new Bookmark(account,
                                         "http://bookmark.com/3/" + a, "A Wrinkle in Time"));
